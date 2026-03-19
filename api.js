354bf71d55ab261b7b461953f7c0972b39e513d2
kabelo-mastermind <0604749776kb@gmail.com>
2026-03-19 11:39:48 +0200

    update

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 // export const api_local = 'http://localhost:3000/api/';
 // export const api = 'https://tech-wise-server-brown.vercel.app/api/';
 // export const BASE_URL = 'https://socketserver-k62n.onrender.com';
-export const api  = 'http://10.2.43.113:3000/api/';
-export const BASE_URL  = 'http://10.2.43.113:3000';
+export const api  = 'https://api.nthomekp.com/api/';
+export const BASE_URL  = 'https://api.nthomekp.com'; 
 
 
